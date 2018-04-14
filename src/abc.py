4c3480fdeec7c2df06100f620c4f09abd34821e7
ojas97 <ojasagarwal97@gmail.com>
2018-04-14 16:22:12 +0530

    modularized

--- src/abc.py
+++
@@ -1,7 +0,0 @@
-import glob
-path = '//home//ojas//PycharmProjects//Term_Project//src//Images//*.jpg'
-files = glob.glob(path)
-known_face_encoding=[]
-for imag in files:
-    name=imag[57:-4]
-    print name
